5d6bac817caf055a51a11d14d2f051a666b0a4d2
samir <samalam281@gmail.com>
2023-11-05 00:59:55 +0530

    folderstructuredone1

--- eduquik/src/App.js
+++ eduquik/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
-    
+    <About/>
     </div>
   );
 }
